5cb59b0263ddaff266724f0f53bbf22a3662b004
AlexKnauth <alexander@knauth.org>
2015-03-21 12:07:04 -0400

    add set! to def.rkt

--- sweet-exp-utils/def.rkt
+++ sweet-exp-utils/def.rkt
@@ -5,10 +5,16 @@ provide all-defined-out()
 
 require racket/match
         racket/block
+        racket/contract/base
         math/flonum
         rackunit
         syntax/parse/define
+        only-in racket/base [set! rkt:set!]
         defpat
+        my-cond/iffy
+        for-syntax racket/base
+                   syntax/parse
+                   syntax/srcloc
 
 define-simple-macro
   def a:expr (~datum =) b:expr ...+
@@ -19,6 +25,27 @@ define-simple-macro
   defm a:expr (~datum =) b:expr ...+
   match-define a
     block b ...
+
+define-syntax set!
+  syntax-parser
+    (set! x:id (~datum =) v:expr)
+      syntax (rkt:set! x v)
+    (set! x:id (~and s (~datum +=)) v:expr)
+      #:with +
+      datum->syntax(#'s '+ update-source-location(#'s #:span 1) #'s)
+      syntax (set! x = {x + v})
+    (set! x:id (~and s (~datum -=)) v:expr)
+      #:with -
+      datum->syntax(#'s '- update-source-location(#'s #:span 1) #'s)
+      syntax (set! x = {x - v})
+    (set! x:id (~and s (~datum *=)) v:expr)
+      #:with *
+      datum->syntax(#'s '* update-source-location(#'s #:span 1) #'s)
+      syntax (set! x = {x * v})
+    (set! x:id (~and s (~datum /=)) v:expr)
+      #:with /
+      datum->syntax(#'s '/ update-source-location(#'s #:span 1) #'s)
+      syntax (set! x = {x / v})
 
 define-simple-macro
   chk a:expr (~datum =) b:expr
@@ -44,4 +71,22 @@ module+ test
   chk f2() = 2
   chk f2(3) = 3
   chk {3 ^ 2} = 9
+  test-case "set!"
+    def x = 1
+    chk x = 1
+    set! x += 2
+    chk x = 3
+    def add = +
+    block
+      def (+ . args) =
+        my-cond
+          if andmap(number? args)
+            apply(add args)
+          else-if andmap((listof number?) args)
+            apply(map add args)
+          else
+            error('+ "expected all numbers or all lists, given ~v" args)
+      def y = '(1 2 3)
+      set! y += '(4 5 6)
+      chk y = '(5 7 9)
 
